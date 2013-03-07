423ac0f46d2ffab7e60546f268d5aafc59221e0a
Tom Meier <tom@venombytes.com>
2013-03-07 11:53:23 +1100

    Updated specs for split up tasks

---
+++ unit/releaseable-test.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env roundup
+source ./spec/scripts/script_spec_helper.sh
+source ./script/support/releaseable.sh
+
+describe "releaseable : unit"
+
+after() {
+  remove_sandbox
+}
+#validate_inputs()
+
+it_fails_on_validate_inputs_with_no_version_type() {
+  should_fail "$(validate_version_type)"
+}
+
+it_fails_on_validate_inputs_with_invalid_version_type() {
+  should_fail "$(validate_version_type "invalid_type")"
+}
+
+it_passes_on_validate_inputs_with_major_version_type() {
+  should_succeed $(validate_version_type "major")
+}
+
+it_passes_on_validate_inputs_with_minor_version_type() {
+  should_succeed $(validate_version_type "minor")
+}
+
+it_passes_on_validate_inputs_with_patch_version_type() {
+  should_succeed $(validate_version_type "patch")
+}
+
+
+#ensure_git_directory()
+
+it_fails_on_ensure_git_directory_with_no_git() {
+  enter_sandbox
+  rm -rf .git
+
+  should_fail "$(ensure_git_directory)"
+}
+
+it_passes_on_ensure_git_directory_with_git_directory() {
+  enter_sandbox
+  mkdir -p .git
+
+  should_succeed $(ensure_git_directory)
+}
+
+# function versioning_prefix() {
+#   if [[ $2 ]]; then
+#     echo "${1}/{2}"
+#   else
+#     echo "${1}"
+#   fi;
+# }
+
+#versioning_prefix()
+
+it_uses_versioning_prefix_to_generate_concatenated_prefix() {
+   output=$(versioning_prefix SomEthing VER)
+   test "$output" = "SomEthing/VER"
+}
+
+it_uses_versioning_prefix_to_generate_singular_prefix() {
+  output=$(versioning_prefix MY-RELEASES)
+  test "$output" = "MY-RELEASES"
+}
+
+#get_release_tags()
+
+it_uses_get_release_tags_to_return_all_tags_with_no_pattern_ordered_by_alpha() {
+  generate_sandbox_tags
+
+  output=$(get_release_tags)
+  test "$output" = "random_tag_1
+random_tag_2
+random_tag_3
+release/production/v1.0.9
+release/production/v3.0.9
+release/staging/v1.0.2
+release/staging/v2.0.3
+release/v1.0.5
+release/v1.0.6"
+}
+
+it_uses_get_release_tags_to_return_tags_matching_a_given_pattern() {
+  generate_sandbox_tags
+
+  output=$(get_release_tags random)
+  test "$output" = "random_tag_1
+random_tag_2
+random_tag_3"
+
+  output=$(get_release_tags release/production)
+  test "$output" = "release/production/v1.0.9
+release/production/v3.0.9"
+}
+
+
+
+
+
+
