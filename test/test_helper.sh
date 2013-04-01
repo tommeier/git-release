8aeafba43240103d60960db04c89207e50a4b94c
Tom Meier <tom@venombytes.com>
2013-04-01 15:34:47 +1100

    Revert "Remove brackets on functions test run"
    This reverts commit b877833e65f16f980ec75766bb88812e63fb3632.

--- test/test_helper.sh
+++ test/test_helper.sh
@@ -2,12 +2,12 @@
 
 #Script spec helpers
 
-function script_directory {
+function script_directory() {
   "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 }
 
 #Search argument 1 for substring in argument 2
-function search_substring {
+function search_substring() {
   if echo "$1" | grep -q "$2"; then
     echo 'found'
   else
@@ -15,7 +15,7 @@ function search_substring {
   fi;
 }
 
-function should_succeed {
+function should_succeed() {
   if [[ $? = 0 ]]; then
     return 0
   else
@@ -23,11 +23,11 @@ function should_succeed {
   fi;
 }
 
-function should_fail {
+function should_fail() {
   ! should_succeed
 }
 
-function file_should_exist {
+function file_should_exist() {
   if [[ -f $1 ]];
   then
     return 0;
@@ -36,22 +36,22 @@ function file_should_exist {
   fi;
 }
 
-function file_should_not_exist {
+function file_should_not_exist() {
   ! file_should_exist $1
 }
 
-function enter_sandbox {
+function enter_sandbox() {
   __DIR__="$PWD"
   rm -rf .sandbox
   mkdir -p .sandbox
   cd .sandbox
 }
 
-function remove_sandbox {
+function remove_sandbox() {
   rm -rf .sandbox
 }
 
-function generate_git_repo {
+function generate_git_repo() {
   enter_sandbox
   git init
   touch 'commit_1'
@@ -59,7 +59,7 @@ function generate_git_repo {
   git commit -am "Initial Commit"
 }
 
-function generate_sandbox_tags {
+function generate_sandbox_tags() {
   if [[ ! -f '.git' ]]; then
     #Generate git repo & enter sandbox
     generate_git_repo
