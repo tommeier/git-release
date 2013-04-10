34ff387061cc9d3f3d84e2f680aa0261fef9a95a
Tom Meier <tom@venombytes.com>
2013-04-10 10:02:16 +1000

    Move arguments into lanuage specific file for easier abstraction of releaseable into other languages. I'm not convinced yet, as the code looks horrible. Future refactor required

--- support/language-bash.sh
+++ support/language-bash.sh
@@ -3,17 +3,17 @@
 #Language specific naming, to be overridden by whichever language is being used
 
 ARGA=" " #Argument assigner. space or = for assigning arguments
-
+ARGP="-" #Argument prefix (used in help text display such as '-' for '-v')
 #Arguments for running script
-ARG_VERSION="-v"
-ARG_DEPLOYED_TAG="-d"
-ARG_PREFIX="-p"
-ARG_START="-s"
-ARG_FINISH="-f"
-ARG_APPEND="-A"
-ARG_PULL_REQUESTS="-P"
-ARG_CHANGELOG="-C"
-ARG_VERSION_FILE="-V"
-ARG_HELP_TEXT="-h"
+ARG_VERSION="v"
+ARG_DEPLOYED_TAG="d"
+ARG_PREFIX="p"
+ARG_START="s"
+ARG_FINISH="f"
+ARG_APPEND="A"
+ARG_PULL_REQUESTS="P"
+ARG_CHANGELOG="C"
+ARG_VERSION_FILE="V"
+ARG_HELP_TEXT="h"
 
 
