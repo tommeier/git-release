943028e0ac80dd52a5c59449edcc24fd431c4909
Tom Meier <tom@venombytes.com>
2013-04-12 08:44:48 +1000

    Refactor releaseable to use an arg_for helper for assignment of values and display

--- support/language-bash.sh
+++ support/language-bash.sh
@@ -2,12 +2,12 @@
 
 #Language specific naming, to be overridden by whichever language is being used
 
-ARGA=" " #Argument assigner. space or = for assigning arguments
-ARGP="-" #Argument prefix (used in help text display such as '-' for '-v')
+ARG_ASSIGNER=" " #Argument assigner. space or = for assigning arguments
+ARG_PREFIX="-" #Argument prefix (used in help text display such as '-' for '-v')
 #Arguments for running script
 ARG_VERSION="v"
 ARG_DEPLOYED_TAG="d"
-ARG_PREFIX="p"
+ARG_RELEASE_PREFIX="p"
 ARG_START="s"
 ARG_FINISH="f"
 ARG_APPEND="A"
@@ -16,4 +16,14 @@ ARG_CHANGELOG="C"
 ARG_VERSION_FILE="V"
 ARG_HELP_TEXT="h"
 
+#Display arg with language specific assigner and prefix
+arg_for() {
+  local arg="$1"
+  local val="$2"
+  if [[ $val != '' ]]; then
+    echo "${ARG_PREFIX}${1}${ARG_ASSIGNER}${2}"
+  else
+    echo "${ARG_PREFIX}${1}"
+  fi;
+}
 
